1d24866a4348c6cf3607173b8503503841aeb997
Laurie Hannon <laurie.hannon@sftsrc.com>
2018-05-20 20:50:08 -0700

    Initial Commit

---
+++ DirectLineBot/Controllers/MessagesController.cs
@@ -0,0 +1,79 @@
+﻿namespace DirectLineBot
+{
+    using System;
+    using System.Linq;
+    using System.Net;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using System.Web.Http;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Connector;
+
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        /// <summary>
+        /// POST: api/Messages
+        /// Receive a message from a user and reply to it
+        /// </summary>
+        public async Task<HttpResponseMessage> Post([FromBody]Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new DirectLineBotDialog());
+            }
+            else
+            {
+                await this.HandleSystemMessage(activity);
+            }
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            return response;
+        }
+
+        private async Task HandleSystemMessage(Activity message)
+        {
+            if (message.Type == ActivityTypes.DeleteUserData)
+            {
+                // Implement user deletion here
+                // If we handle user deletion, return a real message
+            }
+            else if (message.Type == ActivityTypes.ConversationUpdate)
+            {
+                if (message.MembersAdded.Any(o => o.Id == message.Recipient.Id))
+                {
+                    ConnectorClient client = new ConnectorClient(new Uri(message.ServiceUrl));
+
+                    var reply = message.CreateReply();
+
+                    reply.Text = "I am the DirectlineBot. I received a message in the MessagesController and my Message.Type is " + message.Type;
+
+                    await client.Conversations.ReplyToActivityAsync(reply);
+                    return;
+                }
+            }
+            else if (message.Type == ActivityTypes.ContactRelationUpdate)
+            {
+                // Handle add/remove from contact lists
+                // Activity.From + Activity.Action represent what happened
+            }
+            else if (message.Type == ActivityTypes.Typing)
+            {
+                // Handle knowing tha the user is typing
+            }
+            else if (message.Type == ActivityTypes.Ping)
+            {
+            }
+
+          
+            ConnectorClient defaultClient = new ConnectorClient(new Uri(message.ServiceUrl));
+
+            var defaultReply = message.CreateReply();
+
+            defaultReply.Text = "I am the DirectlineBot. I received a message in the MessagesController and my Message.Type is " + message.Type;
+
+            await defaultClient.Conversations.ReplyToActivityAsync(defaultReply);
+            
+        }
+    }
+}
